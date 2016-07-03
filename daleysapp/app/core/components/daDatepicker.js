bc024e1bc1173b1c804d64a222cdeea9bf50a683
Daley <ctlncardwell@gmail.com>
2016-07-03 15:55:40 -0400

    refactor(app): datepicker is now a component, delete extra code in importantTasks and lessImportantTasks folders, change format of Today and Clear buttons

---
+++ daleysapp/app/core/components/daDatepicker.js
@@ -0,0 +1,62 @@
+﻿(function () {
+    'use strict';
+    angular.module('app').component('daDatepicker', {
+        templateUrl: 'app/core/components/daDatepicker.html',
+        controller: Controller,
+        bindings: {
+            selectedDate: '='
+        },
+        restrict: 'E'
+    });
+
+    function Controller($scope) {
+        var vm = this;
+
+        vm.clear = clear;
+
+
+
+        vm.dateOptions = {
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+       
+        vm.dateOptions.formatYear = '';
+        vm.formatYear = '';
+
+
+        vm.format = 'MM/dd/yyyy';
+
+        vm.open = open;
+        vm.popup = {
+            opened: false
+        };
+        vm.today = today;
+
+        init();
+
+        function init() {
+            vm.test = {};
+            vm.test.formatYear = '';
+            var test = {};
+            test.formatYear = '';
+            console.log(vm.formatYear);
+        }
+
+        function clear() {
+            console.log(vm.test.formatYear);
+            vm.selectedDate = undefined;
+        }
+
+        function open() {
+            vm.popup.opened = true;
+        }
+
+        function today() {
+            console.log('today function');
+            vm.selectedDate = new Date();
+        }
+
+    }
+})();
